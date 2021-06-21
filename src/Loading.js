63460b7d7b4bc0a7250962e3ca4bd7559399148b
nAzmulHudaa <nhr.developer@gmail.com>
2021-06-22 01:24:49 +0600

    learned a lot of things and completed the project

---
+++ src/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+      <div className="text-center">
+         <div class="spinner-border " style={{width: '3rem', height: '3rem' }}role="status">
+    <span class="sr-only"></span>
+  </div>
+
+      </div>
+  );
+};
+
+export default Loading;
+
